97c780121baf285578dd061c3832ba839c72072f
PhoVic14 <91669693+PhoVic14@users.noreply.github.com>
2023-06-01 16:09:02 +0200

    Ajout de l'examen
    Ajout de l'examen

---
+++ CeUAA11_2023_VictorPholien/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+namespace CeUAA11_2023_VictorPholien
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string phClaire;
+            string phClef;
+            string MatVigenere;
+            int a;
+            int b;
+            string matAffine;
+
+            methodes MesOutils = new methodes();
+
+            Console.WriteLine("CRYPTAGE");
+            Console.WriteLine("========");
+            bool continuer = true;
+
+            
+            while (continuer) //Boucle pour pouvoir recommencer
+            {
+                Console.WriteLine("Choisissez parmi les options suivantes");
+                Console.WriteLine("1 - Cryptage de Viginère");
+                Console.WriteLine("2 - Cryptage avec la methode affine");
+                int choix = int.Parse(Console.ReadLine());
+
+                if( choix == 1) //Si l'utilisateur choisi le Cryptage de Viginère
+                {
+                    Console.WriteLine("Encodez la phrase a crypter");
+                    phClaire = Console.ReadLine();
+                    Console.WriteLine("Vous avez choisi la methode Viginère");
+                    Console.WriteLine("------------------------------------");
+                    Console.WriteLine("Encodez la clé a utiliser dans la methode Viginaire");
+                    phClef = Console.ReadLine();
+                    MesOutils.CryptVigenere( phClaire,  phClef, out MatVigenere);
+                    Console.WriteLine("Voici la matrice de Cryptage");
+                    Console.WriteLine("----------------------------");
+                    Console.WriteLine(MatVigenere);
+                }
+
+                else if (choix == 2) //Si l'utilisateur choisi le Cryptage avec la méthode affine
+                {
+                    Console.WriteLine("Encodez la phrase a crypter");
+                    phClaire = Console.ReadLine();
+                    Console.WriteLine("Donnez la valeur de a (impaire entre 1 et 25 sauf 13)");
+                    a = int.Parse(Console.ReadLine());
+                    Console.WriteLine("Donnez la valeur de b");
+                    b = int.Parse(Console.ReadLine());
+                    MesOutils.CryptAffine(phClaire, a, b, out matAffine);
+                    Console.WriteLine("Résultat du cryptage par la methode affine");
+                    Console.WriteLine("----------------------------");
+                    Console.WriteLine(matAffine);
+                }
+
+
+                Console.WriteLine("\nVoulez-vous continuer ? (O/N)"); //Demande pour relancer le programme
+                string reponse = Console.ReadLine().ToUpper();
+                continuer = (reponse == "O" || reponse == "YES");
+            }
+        }
+    }
+}
---
+++ CeUAA11_2023_VictorPholien/methodes.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CeUAA11_2023_VictorPholien
+{
+    public struct methodes
+    {
+
+        /// <summary>
+        /// Cryptage Vigenere
+        /// </summary>
+        /// <param name="phClaire">Texte en claire</param>
+        /// <param name="phClef">clef pour crypter le texte en claire</param>
+        /// <param name="MatVigenere">Matrice</param>
+        public void CryptVigenere(string phClaire, string phClef, out string[,] MatVigenere)
+        {
+            int i;
+            int j;
+            int codeAscii;
+
+            MatVigenere = new string[4, phClaire.Length];
+            j = 0;
+            for (i = 0; i > phClaire.Length; i++)
+            {
+                MatVigenere[1, i] = phClaire[i].ToString();
+
+                if (j = phClef.Parse)
+                {
+                    j = 0;
+                    MatVigenere[1, i] = phClef[j].ToString();
+                    MatVigenere[2, i] = (((int)phClef[j]) - 65).ToString();
+                    if (((int)phClaire[i] + int.Parse(MatVigenere[2, 1]) <= 90))
+                    {
+                        codeAscii = (int)char.Parse(MatVigenere[0, i]) + int.Parse(MatVigenere[2, i]);
+                    }
+                    else
+                    {
+                        codeAscii = (int)char.Parse(MatVigenere[0, i]) + int.Parse(MatVigenere[2, i]) - 26;
+                    }
+                    MatVigenere[3, i] = Convert.ToChar(codeAscii).ToString();
+                    j = j + 1;
+                }
+            }
+        }
+
+
+        /// <summary>
+        /// Cryptage Affine
+        /// </summary>
+        /// <param name="phClaire">Phrase en claire</param>
+        /// <param name="a">Nombre a dans la formule</param>
+        /// <param name="b">Nombre b dans la formule</param>
+        /// <param name="matAffine">matrice</param>
+        public void CryptAffine(string phClaire, int a, int b, string[,] matAffine)
+        {
+            int i;
+            int x;
+            int y;
+
+            matAffine = new string[4, phClaire.Length];
+            for (i = 0; i > phClaire.Length - 1; i++)
+            {
+                matAffine[2, i] = phClaire[i].ToString();
+                x = ((int)phClaire[i] - 65);
+                matAffine[1, i] = "x";
+                y = (a * x + b) % 26;
+                matAffine[2, i] = "y";
+                matAffine[3, i] = y + 65.ToString();
+            }
+        }
+    }
+}
